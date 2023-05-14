bd3f9b4101cca854de3c77a1342210287e30a421
Szabó András <szabag@gmail.com>
2023-05-14 12:56:57 +0200

    loss sampling to train on batch of style ref images

--- 08_style_combination_loss.py
+++ 08_style_combination_loss.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 import time
@@ -7,74 +8,19 @@ import tensorflow as tf
 from keras.applications import vgg19
 from tensorflow import keras
 
-BASE_IMG_PATH = "input/myplace2.jpg"
 OUT_FOLDER = "output/style_tests/"
 
-width, height = keras.preprocessing.image.load_img(BASE_IMG_PATH).size
-img_nrows = 400
-img_ncols = int(width * img_nrows / height)
 style_layer_names = [
-    "block1_conv1", "block2_conv1",
-    "block3_conv1", "block4_conv1", "block5_conv1",
+    ("block1_conv1", 0.4),
+    ("block2_conv1", 0.3),
+    ("block3_conv1", 0.2),
+    ("block4_conv1", 0.05),
+    ("block5_conv1", 0.05),
 ]
 content_layer_name = "block5_conv2"
 
 
-def preprocess_image(image_path):
-    # Util function to open, resize and format pictures into appropriate tensors
-    img = keras.preprocessing.image.load_img(
-        image_path, target_size=(img_nrows, img_ncols)
-    )
-    img = keras.preprocessing.image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = vgg19.preprocess_input(img)
-    return tf.convert_to_tensor(img)
-
-
-def deprocess_image(x):
-    # Util function to convert a tensor into a valid image
-    x = x.reshape((img_nrows, img_ncols, 3))
-    # Remove zero-center by mean pixel
-    x[:, :, 0] += 103.939
-    x[:, :, 1] += 116.779
-    x[:, :, 2] += 123.68
-    # 'BGR'->'RGB'
-    x = x[:, :, ::-1]
-    x = np.clip(x, 0, 255).astype("uint8")
-    return x
-
-
-def gram_matrix(x):
-    x = tf.transpose(x, (2, 0, 1))
-    features = tf.reshape(x, (tf.shape(x)[0], -1))
-    gram = tf.matmul(features, tf.transpose(features))
-    return gram
-
-
-def style_loss(style, combination):
-    S = gram_matrix(style)
-    C = gram_matrix(combination)
-    channels = 3
-    size = img_nrows * img_ncols
-    return tf.reduce_sum(tf.square(S - C)) / (4.0 * (channels**2) * (size**2))
-
-
-def content_loss(base, combination):
-    return tf.reduce_sum(tf.square(combination - base))
-
-
-def total_variation_loss(x):
-    a = tf.square(
-        x[:, : img_nrows - 1, : img_ncols - 1, :] - x[:, 1:, : img_ncols - 1, :]
-    )
-    b = tf.square(
-        x[:, : img_nrows - 1, : img_ncols - 1, :] - x[:, : img_nrows - 1, 1:, :]
-    )
-    return tf.reduce_sum(tf.pow(a + b, 1.25))
-
-
 class StyleTransfer:
-
     iterations = 4000
     style_update_freq = 100
     save_freq = 100
@@ -84,6 +30,7 @@ class StyleTransfer:
     sampling_mode = "SEQ"  # RAND
 
     def __init__(self, prefix,
+                 base_img_path,
                  iterations,
                  total_variation_weight,
                  style_weight,
@@ -97,6 +44,12 @@ class StyleTransfer:
                  sampling_mode):
 
         print(f"Initializing style transfer: {prefix}")
+
+        self.base_img_path = base_img_path
+
+        self.width, self.height = keras.preprocessing.image.load_img(self.base_img_path).size
+        self.img_nrows = 400
+        self.img_ncols = int(self.width * self.img_nrows / self.height)
 
         self.prefix = prefix
         self.iterations = iterations
@@ -124,6 +77,53 @@ class StyleTransfer:
         self.out_folder = OUT_FOLDER + prefix + "/"
         os.mkdir(self.out_folder)
 
+    def preprocess_image(self, image_path):
+        # Util function to open, resize and format pictures into appropriate tensors
+        img = keras.preprocessing.image.load_img(
+            image_path, target_size=(self.img_nrows, self.img_ncols)
+        )
+        img = keras.preprocessing.image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = vgg19.preprocess_input(img)
+        return tf.convert_to_tensor(img)
+
+    def deprocess_image(self, x):
+        # Util function to convert a tensor into a valid image
+        x = x.reshape((self.img_nrows, self.img_ncols, 3))
+        # Remove zero-center by mean pixel
+        x[:, :, 0] += 103.939
+        x[:, :, 1] += 116.779
+        x[:, :, 2] += 123.68
+        # 'BGR'->'RGB'
+        x = x[:, :, ::-1]
+        x = np.clip(x, 0, 255).astype("uint8")
+        return x
+
+    def gram_matrix(self, x):
+        x = tf.transpose(x, (2, 0, 1))
+        features = tf.reshape(x, (tf.shape(x)[0], -1))
+        gram = tf.matmul(features, tf.transpose(features))
+        return gram
+
+    def style_loss(self, style, combination):
+        S = self.gram_matrix(style)
+        C = self.gram_matrix(combination)
+        channels = 3
+        size = self.img_nrows * self.img_ncols
+        return tf.reduce_sum(tf.square(S - C)) / (4.0 * (channels ** 2) * (size ** 2))
+
+    def content_loss(self, base, combination):
+        return tf.reduce_sum(tf.square(combination - base))
+
+    def total_variation_loss(self, x):
+        a = tf.square(
+            x[:, : self.img_nrows - 1, : self.img_ncols - 1, :] - x[:, 1:, : self.img_ncols - 1, :]
+        )
+        b = tf.square(
+            x[:, : self.img_nrows - 1, : self.img_ncols - 1, :] - x[:, : self.img_nrows - 1, 1:, :]
+        )
+        return tf.reduce_sum(tf.pow(a + b, 1.25))
+
     def compute_loss(self, combination_image, base_image, style_reference_image):
         input_tensor = tf.concat(
             [base_image, style_reference_image, combination_image], axis=0
@@ -136,17 +136,17 @@ class StyleTransfer:
         layer_features = features[content_layer_name]
         base_image_features = layer_features[0, :, :, :]
         combination_features = layer_features[2, :, :, :]
-        loss = loss + self.content_weight * content_loss(
+        loss = loss + self.content_weight * self.content_loss(
             base_image_features, combination_features
         )
-        for layer_name in style_layer_names:
+        for layer_name, weight in style_layer_names:
             layer_features = features[layer_name]
             style_reference_features = layer_features[1, :, :, :]
             combination_features = layer_features[2, :, :, :]
-            sl = style_loss(style_reference_features, combination_features)
-            loss += (self.style_weight / len(style_layer_names)) * sl
+            sl = self.style_loss(style_reference_features, combination_features)
+            loss += weight * sl # (self.style_weight / len(style_layer_names))
 
-        loss += self.total_variation_weight * total_variation_loss(combination_image)
+        loss += self.total_variation_weight * self.total_variation_loss(combination_image)
         return loss
 
     def compute_loss_combination(self, combination_image, base_image, style_reference_images):
@@ -164,13 +164,21 @@ class StyleTransfer:
 
     def _get_all_style_ref(self):
         for f in self.styles:
-            yield preprocess_image(f)
+            yield self.preprocess_image(f)
+
+    def _save_run_params(self):
+        run_params = {}
+        for name, val in self.__dict__.items():
+            if type(val) in [str, int, float]:
+                run_params[name] = val
+        with open(self.out_folder + "params.json", "w") as f:
+            json.dump(run_params, f, indent=4)
 
     def train(self):
         st = time.time()
-        base_image = preprocess_image(BASE_IMG_PATH)
+        base_image = self.preprocess_image(self.base_img_path)
         style_reference_images = list(self._get_all_style_ref())
-        combination_image = tf.Variable(preprocess_image(BASE_IMG_PATH))
+        combination_image = tf.Variable(self.preprocess_image(self.base_img_path))
 
         for i in range(1, self.iterations + 1):
             loss, grads = self.compute_loss_and_grads(
@@ -179,10 +187,11 @@ class StyleTransfer:
             self.optimizer.apply_gradients([(grads, combination_image)])
             if i % self.save_freq == 0:
                 print("Iteration %d: loss=%.2f" % (i, loss))
-                img = deprocess_image(combination_image.numpy())
+                img = self.deprocess_image(combination_image.numpy())
                 fname = self.out_folder + "at_iteration_%d.png" % i
                 keras.preprocessing.image.save_img(fname, img)
-        print(f"{self.prefix} training ended in {time.time() -st} sec")
+        self._save_run_params()
+        print(f"{self.prefix} training ended in {time.time() - st} sec")
 
 
 if __name__ == "__main__":
@@ -190,8 +199,16 @@ if __name__ == "__main__":
     #               iterations=1000, total_variation_weight=1e-6, style_weight=1e-6,
     #               content_weight=2.5e-8, styles_path="input/styles/test2/",
     #               initial_learning_rate=90.0, decay_steps=100, decay_rate=0.96).train()
-    StyleTransfer(prefix="test_11_lowpoly", style_update_freq=100, save_freq=100,
-                  iterations=1000, total_variation_weight=1e-6, style_weight=1e-6,
-                  content_weight=2.5e-8, styles_path="input/styles/test2/",
-                  initial_learning_rate=100.0, decay_steps=100, decay_rate=0.96,
-                  sampling_mode="RAND").train()
+    jobs = [
+        StyleTransfer(prefix="test_107_marq", base_img_path="input/myplace2.jpg",
+                      style_update_freq=100, save_freq=100,
+                      iterations=1000, total_variation_weight=1e-6, style_weight=1e-7,
+                      content_weight=2.5e-8, styles_path="input/styles/test7/",
+                      initial_learning_rate=90.0, decay_steps=100, decay_rate=0.99,
+                      sampling_mode="RAND")
+    ]
+    for st in jobs:
+        try:
+            st.train()
+        except Exception as e:
+            print(e)
